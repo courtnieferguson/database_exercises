b38fe08ecee5fb3b7d986f32ab269581e1b52e72
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2021-08-25 10:11:08 -0500

    got it

--- delete_exercise.sql
+++ delete_exercise.sql
@@ -1,1 +1,16 @@
+USE codeup_test_db;
 
+DELETE
+FROM albums
+WHERE release_date > 1991;
+
+DELETE
+FROM albums
+WHERE genre = 'Disco';
+
+DELETE
+FROM albums
+WHERE artist = 'Whitney Houston';
+
+SELECT *
+FROM albums;
